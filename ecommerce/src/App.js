d1db8b366c77ad4fd3cfefbda3aff506b9995eab
Blin75 <144045160+Blin75@users.noreply.github.com>
2025-02-12 20:10:51 +0100

    .

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [products, setProducts] = useState([]);
+  const [sampleProduct, setSampleProduct] = useState(undefined);
+
+  useEffect(() => {
+    fetch("/api/products.json")
+    .then(res => res.json())
+    .then(res => setProducts(res));
+  }, []);
+
+  useEffect(() => {
+    fetch("/api/products/1.json")
+    .then(res => res.json())
+    .then(res => setSampleProduct(res));
+  }, []);
+  
+  return <div>
+    <h1 className="text-3xl font-bold text-center mt-3 mb-3">App</h1>
+    {sampleProduct && <div>The first product is: {sampleProduct.name}</div>}
+    <ul>
+      {products.map(p => <li key={p.id}>
+        <figure>
+          <img src={`/images/${p.imageUrl}`}></img>
+          <figcaption>{p.name} - {p.description}</figcaption>
+        </figure>
+      </li>)}
+    </ul>
+  </div>
 }
 
 export default App;
